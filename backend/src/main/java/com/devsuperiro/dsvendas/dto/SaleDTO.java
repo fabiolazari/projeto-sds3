b4f32f41456bee8335b3fb151ec43d11deabec8f
Fabio Lazari Costa <lazari.developer@gmail.com>
2021-05-07 12:35:26 -0300

    Pagination

--- backend/src/main/java/com/devsuperiro/dsvendas/dto/SaleDTO.java
+++ backend/src/main/java/com/devsuperiro/dsvendas/dto/SaleDTO.java
@@ -1,36 +1,24 @@
-package com.devsuperiro.dsvendas.entities;
+package com.devsuperiro.dsvendas.dto;
 
 import java.time.LocalDate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import com.devsuperiro.dsvendas.entities.Sale;
 
-@Entity
-@Table(name = "tb_sales")
-public class Sale {
+public class SaleDTO {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Integer visited;
 	private Integer deals;
 	private Double amount;
 	private LocalDate date;
 	
-	@ManyToOne
-	@JoinColumn(name = "seller_id")
-	private Seller seller;
+	private SellerDTO seller;
 	
-	public Sale() {
+	public SaleDTO() {
 		
 	}
 
-	public Sale(Long id, Integer visited, Integer deals, Double amount, LocalDate date, Seller seller) {
+	public SaleDTO(Long id, Integer visited, Integer deals, Double amount, LocalDate date, SellerDTO seller) {
 		super();
 		this.id = id;
 		this.visited = visited;
@@ -38,6 +26,16 @@ public class Sale {
 		this.amount = amount;
 		this.date = date;
 		this.seller = seller;
+	}
+	
+	public SaleDTO(Sale entity) {
+
+		id = entity.getId();
+		visited = entity.getVisited();
+		deals = entity.getDeals();
+		amount = entity.getAmount();
+		date = entity.getDate();
+		seller = new SellerDTO(entity.getSeller());
 	}
 
 	public Long getId() {
@@ -80,11 +78,11 @@ public class Sale {
 		this.date = date;
 	}
 
-	public Seller getSeller() {
+	public SellerDTO getSeller() {
 		return seller;
 	}
 
-	public void setSeller(Seller seller) {
+	public void setSeller(SellerDTO seller) {
 		this.seller = seller;
 	}
 }
---
+++ backend/src/main/java/com/devsuperiro/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperiro.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperiro.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
